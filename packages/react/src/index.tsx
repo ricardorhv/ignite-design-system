7980b2d3416777dadf8f2ef8d300fe4e36c51b0c
Ricardo Vinciguerra <ricardorhv.dev@gmail.com>
2024-02-16 16:26:37 -0300

    feat: install storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,9 +1,9 @@
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
   backgroundColor: '$ignite500',
+  padding: '$3',
+  borderRadius: '$md',
+  border: 'none',
 })
-export function App() {
-  return <Button>Hello</Button>
-}
